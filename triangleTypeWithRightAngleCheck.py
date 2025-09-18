b4950b5bd6e0abc68463ac09498cd3c796b6a2f2
raajpatre <raajpatre27@gmail.com>
2025-09-18 16:56:19 +0530

    Update triangleTypeWithRightAngleCheck.py

--- triangleTypeWithRightAngleCheck.py
+++ triangleTypeWithRightAngleCheck.py
@@ -2,12 +2,8 @@ a,b,c=map(int,input().split())
 if(a<b+c and b<c+a and c<a+b):
     if(a==b and b==c):
         print("Equilateral triangle")
-        if(a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b):
-            print("Right angled Triangle")
     elif(a==b or b==c or c==a):
         print("Isosceles triangle")
-        if(a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b):
-            print("Right angled Triangle")
     else:
         print("Scalene triangle")
         if(a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b):
